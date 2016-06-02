27a853be7d80dbc46c85adeb933c170c4b62dba9
kgryte <kgryte@gmail.com>
2016-06-01 22:25:11 -0700

    Add debug statement

--- tools/install/julia.sh
+++ tools/install/julia.sh
@@ -43,9 +43,10 @@ install_julia() {
 		hdiutil unmount $volume -quiet
 		rm $tmp_file
 
-		# Place the Julia executable in our path:
+		# Permanently add Julia to the executable path:
 		echo "export PATH=$apps_folder/`echo $app | awk -F/ '{print $NF}'`/Contents/Resources/julia/bin/:$PATH" >> ~/.bash_profile
 		source ~/.bash_profile
+		cat ~/.bash_profile
 	else
 		# Add personal package archives (PPAs) for updating to the latest stable Julia versions...
 		echo 'Adding PPAs...'
